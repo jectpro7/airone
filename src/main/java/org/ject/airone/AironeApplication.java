f5e471dfbfc900b3f21b25e4c2c832410473090d
jectpro <ject7pro@gmail.com>
2021-05-06 23:23:49 +0800

    feat: init

---
+++ src/main/java/org/ject/airone/AironeApplication.java
@@ -0,0 +1,13 @@
+package org.ject.airone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AironeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AironeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ject/airone/AironeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ject.airone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AironeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
